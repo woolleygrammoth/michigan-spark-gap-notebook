340c141aa6053fdca1ceb8582de2ce0ac066b925
Graham Woolley <gwoolley@berkeley.edu>
2026-01-07 16:56:31 -0800

    Add interactive notebook and data

---
+++ config.py
@@ -0,0 +1,24 @@
+data_year = 2023
+sector = 'Residential Sector'
+state_focus = 'Michigan'
+peer_states = ['Wisconsin', 'Minnesota', 'Indiana', 'Illinois', 'Ohio']
+sectors = [
+    'Residential Sector'
+    , 'Commercial Sector'
+    , 'Industrial Sector'
+    , 'Transportation Sector'
+    , 'All Sectors'
+]
+elec_exp_metric = 'Electricity Expenditures per Customer - Residential Sector'
+exclude_states = [
+    'HI'
+    , 'CA'
+    , 'AK'
+    , 'MA'
+    , 'ME'
+    , 'RI'
+    , 'CT'
+    , 'NH'
+    , 'VT'
+    ]
+building_type_options = ['mobile_home', 'single-family_detached', 'single-family_attached', 'multi-family_with_5plus_units', 'multi-family_with_2_-_4_units']
